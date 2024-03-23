2a381bad63f2872ffd49abfec074cda29e0e3c21
Edon <edon@valdman.works>
2024-03-23 13:08:03 -0500

    Replaced AppDelegate with cross-platform

--- SwiftPong/SwiftPongApp.swift
+++ SwiftPong/SwiftPongApp.swift
@@ -9,11 +9,39 @@ import SwiftUI
 
 @main
 struct SwiftPongApp: App {
-    @NSApplicationDelegateAdaptor(SwiftPongAppDelegate.self) var appDelegate
-
+    @AppDeleteAdaptor
+    private var appDelegate: AppDelegate
+    
     var body: some Scene {
         WindowGroup {
             GameView()
         }
     }
 }
+
+#if os(macOS)
+typealias AppDelegateProtocol = NSApplicationDelegate
+typealias AppDeleteAdaptor = NSApplicationDelegateAdaptor
+#else
+typealias AppDelegateProtocol = UIApplicationDelegate
+typealias AppDeleteAdaptor = UIApplicationDelegateAdaptor
+#endif
+
+class AppDelegate: NSObject, AppDelegateProtocol, ObservableObject {
+    
+}
+
+#if os(macOS)
+extension AppDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
+#else
+extension AppDelegate {
+    //    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    //        // Override point for customization after application launch.
+    //        return true
+    //    }
+}
+#endif
